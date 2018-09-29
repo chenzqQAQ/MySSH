191385ffc5c90f47b3f10679af74379df4a6db61
chenzqQAQ <649211123@qq.com>
2018-09-29 11:25:45 +0800

    Initial commit

---
+++ src/main/java/com/you/bean/User.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C),2015-2018
+ * FileNmae: User
+ * Author:   Administrator
+ * Date:     2018/9/29 11:12
+ * History:
+ * <author> <Time> <version> <desc>
+ * 陈泽群  时间    版本号  描述
+ */
+package com.you.bean;
+
+/**
+ *User
+ *用户
+ *@author 陈泽群
+ *@date 2018/9/29 11:12
+ */
+public class User {
+    private String userName;
+    private String passWord;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/9/29
+  Time: 10:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>主页面</title>
+  </head>
+  <body>
+  准备ssh框架项目
+  </body>
+</html>
